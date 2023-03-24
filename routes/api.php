3dcdd9c7bbd61e55c6ee4da6ec166531fb73a973
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-03-24 18:45:47 +0000

    User: get, update profile, logout

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Authentication;
-
-
+use App\Http\Middleware\Authenticate;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +12,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('auth/register', Authentication\RegisterController::class);
 Route::post('auth/login', Authentication\RegisterController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [Authentication\ProfileController::class, 'show']);
+    Route::put('profile', [Authentication\ProfileController::class, 'update']);
+    Route::put('password', Authentication\PasswordUpdateController::class);
+    Route::put('logout', Authentication\LogoutController::class);
+});
